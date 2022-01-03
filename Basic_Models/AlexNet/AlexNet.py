c2652edc8fd599448d928f0b16a206ceefcf7f4d
JongMin Baek <57121112+Jm-baek@users.noreply.github.com>
2022-01-03 16:57:38 +0900

    Update AlexNet.py

--- Basic_Models/AlexNet/AlexNet.py
+++ Basic_Models/AlexNet/AlexNet.py
@@ -1,1 +1,32 @@
+def AlexNet():
+    inputs = Input(shape=(227, 227, 3))
+        
+    x = Conv2D(96, kernel_size=(11, 11), strides=4, padding='valid')(inputs)
 
+    x = Activation('ReLU')(x)
+    x = MaxPooling2D(pool_size=(3, 3), strides=2)(x)
+    x = local_response_normalization(x)
+
+    x = Conv2D(256, kernel_size=(3, 3), padding='same')(x)
+
+    x = Activation('ReLU')(x)
+    x = MaxPooling2D(pool_size=(3, 3), strides=2)(x)
+    x = local_response_normalization(x)
+
+    x = Conv2D(384, kernel_size=(3, 3), padding='same', strides=1)(x)
+    x = Conv2D(384, kernel_size=(3, 3), padding='same', strides=1)(x)
+    x = Conv2D(256, kernel_size=(1, 1), padding='same', strides=1)(x)
+
+    x = MaxPooling2D(pool_size=(3, 3), strides=2)(x)
+
+    x = Flatten()(x)
+    x = Dense(4096, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(4096, activation='relu')(x)
+    x = Dropout(0.5)(x)
+
+    outputs = Dense(1000, activation='softmax')(x)
+
+    model = Model(inputs=inputs, outputs=outputs)
+
+    return model
